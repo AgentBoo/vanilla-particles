7bbfe6785633fcb24f2a465994b605b288c98615
agentboo <zuzi.han@gmail.com>
2019-03-30 19:56:08 -0400

    many balls

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// tutorial 
+// resources
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Advanced_animations
+// https://www.html5rocks.com/en/tutorials/canvas/performance/
 
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
@@ -9,17 +10,22 @@ const { height, width } = canvas.parentElement.getBoundingClientRect()
 canvas.width = width 
 canvas.height = height 
 
-ctx.fillStyle = '#222'
-
 canvas.ctx = ctx 
+ 
+// https://stackoverflow.com/a/5365036
+function randomColor(){
+	return "#" + ((1<<24)*Math.random()|0).toString(16)
+} 
+
+canvas.ctx.fillStyle = randomColor()
 
 class Ball{
 	constructor(canvas){
-		this.x = 200
-		this.y = 200
-		this.m = 10
-		this.dx = 10 
-		this.dy = 10 
+		this.x = Math.random() * canvas.width  
+		this.y = Math.random() * canvas.height 
+		this.m = Math.random() * 10
+		this.dx = Math.random() * 0.5 
+		this.dy = Math.random() * 0.5
 	}
 
 	get radius(){
@@ -31,27 +37,36 @@ class Ball{
 		canvas.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
 		canvas.ctx.fill()
 	}
+
+	displace(){
+		this.x += this.dx * 2
+		this.y += this.dy * 3
+
+		if(this.x + this.radius + this.dx > canvas.width || this.x + this.radius + this.dx < 2 * this.radius){
+			this.dx = -this.dx 
+		}
+
+		if(this.y + this.radius + this.dy > canvas.height || this.y + this.radius + this.dy < 2 * this.radius){
+			this.dy = -this.dy 
+		}
+	}
 }
 
-// ball instance
-let ball = new Ball(canvas)
+let balls = []
+
+for(let i = 0; i < 10; i++){
+	balls.push(new Ball(canvas))
+}
 
 // render
 let frame; 
 
 function render(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
-	ball.draw()
 
-	ball.x += ball.dx 
-	ball.y += ball.dy 
-
-	if(ball.x + ball.dx > canvas.width || ball.x + ball.dx < 0){
-		ball.dx = -ball.dx 
-	}
-
-	if(ball.y + ball.dy > canvas.height || ball.y + ball.dy < 0){
-		ball.dy = -ball.dy 
+	for(let i = 0; i < balls.length; i++){
+		balls[i].draw()
+		balls[i].displace()
 	}
 
 	frame = window.requestAnimationFrame(render)
@@ -64,4 +79,3 @@ document.addEventListener('click', e => {
 	frame = window.requestAnimationFrame(render)
 })
 
-ball.draw()
