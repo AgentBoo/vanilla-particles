8563cb7d8c8923e86ed58a644ae11b7c3e774c32
agentboo <zuzi.han@gmail.com>
2019-03-31 18:06:12 -0400

    setup for collisions

--- index.js
+++ index.js
@@ -2,99 +2,173 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Advanced_animations
 // https://www.html5rocks.com/en/tutorials/canvas/performance/
 
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-const { height, width } = canvas.parentElement.getBoundingClientRect()
+const { height, width } = canvas.parentElement.getBoundingClientRect();
 
-canvas.width = width 
-canvas.height = height 
+canvas.width = width;
+canvas.height = height;
 
-canvas.ctx = ctx 
- 
+canvas.ctx = ctx;
+
 // https://stackoverflow.com/a/5365036
-function randomColor(){
-	return "#" + ((1<<24)*Math.random()|0).toString(16)
-} 
+function randomColor() {
+	return "#" + (((1 << 24) * Math.random()) | 0).toString(16);
+}
 
-canvas.ctx.fillStyle = randomColor()
+function square(number) {
+	return number * number;
+}
 
-class Ball{
-	constructor(canvas){
-		this.x = Math.random() * canvas.width  
-		this.y = Math.random() * canvas.height 
-		this.m = Math.random() * 10
-		this.dx = Math.random() * 0.5 
-		this.dy = Math.random() * 0.5
+class Ball {
+	constructor(canvas, init) {
+		this.mass = Math.random() * 4 + 2;
+		this.x = init.x || Math.random() * (canvas.width - 40) + 20; // position X
+		this.y = init.y || Math.random() * (canvas.height - 40) + 20; // position Y
+		this.vx = init.vx || Math.random() * 0.4 + 0.2; // velocity X
+		this.vy = init.vy || Math.random() * 0.4 + 0.2; // velocity Y
+
+		this.growth = {
+			enabled: true,
+			radius: 0.1,
+			rate: 1 / this.mass,
+			factor: 3
+		};
 	}
 
-	get radius(){
-		return this.m 
+	// the heavier the ball, the larger the radius
+
+	get radius() {
+		if (this.growth.enabled) {
+			return this.growth.radius;
+		} else {
+			return this.mass;
+		}
 	}
 
-	draw(){
-		canvas.ctx.beginPath()
-		canvas.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
-		canvas.ctx.fill()
+	get diameter() {
+		return 2 * this.radius;
 	}
 
-	displace(){
-		this.x += this.dx * 2
-		this.y += this.dy * 3
+	get outOfBoundsX() {
+		return (
+			this.x + this.radius + this.vx > canvas.width ||
+			this.x + this.radius + this.vx < this.diameter
+		);
+	}
 
-		if(this.x + this.radius + this.dx > canvas.width || this.x + this.radius + this.dx < 2 * this.radius){
-			this.dx = -this.dx 
+	get outOfBoundsY() {
+		return (
+			this.y + this.radius + this.vy > canvas.height ||
+			this.y + this.radius + this.vy < this.diameter
+		);
+	}
+
+	stopGrowth() {
+		this.growth.enabled = false;
+	}
+
+	squareDistanceTo(neighbour) {
+		return square(this.x - neighbour.x) + square(this.y - neighbour.y);
+	}
+
+	grow() {
+		if (this.growth.radius >= this.mass) {
+			return this.stopGrowth();
+		} else {
+			this.growth.radius += this.growth.rate / this.growth.factor;
+		}
+	}
+
+	draw() {
+		if (this.growth.enabled) {
+			this.grow();
 		}
 
-		if(this.y + this.radius + this.dy > canvas.height || this.y + this.radius + this.dy < 2 * this.radius){
-			this.dy = -this.dy 
+		canvas.ctx.beginPath();
+		canvas.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		canvas.ctx.fillStyle = "black";
+		canvas.ctx.fill();
+	}
+
+	displace() {
+		this.x += this.vx;
+		this.y += this.vy;
+
+		if (this.outOfBoundsX) {
+			this.vx = -this.vx;
+		}
+
+		if (this.outOfBoundsY) {
+			this.vy = -this.vy;
 		}
 	}
 }
 
-let balls = []
+function createBalls(N){
+	let balls = []
 
-for(let i = 0; i < 10; i++){
-	balls.push(new Ball(canvas))
+	for (let i = 0; i < N; i++) {
+		balls.push(new Ball(canvas, {}));
+	}
+
+	return balls
 }
+
+let balls = createBalls(8)
 
 // render
-let frame; 
+let frame;
 
-function render(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
+function render() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	for(let i = 0; i < balls.length; i++){
-		let particle = balls[i]
-		
-		particle.draw()
-		particle.displace()
+	for (let i = 0; i < balls.length; i++) {
+		let particle = balls[i];
 
-		for(let j = i+1; j < balls.length; j++){
-			let neighbour = balls[j]
+		if (balls.length < 50) {
+			for (let j = i + 1; j < balls.length; j++) {
+				let neighbour = balls[j];
 
-			let dx = particle.x - neighbour.x 
-			let dy = particle.y - neighbour.y 
-			let ds = Math.sqrt(dx*dx + dy*dy)
+				let sqdist = particle.squareDistanceTo(neighbour) 
 
-			if(ds < particle.radius + neighbour.radius){
-				particle.dx = -particle.dx
-				particle.dy = -particle.dy  
-				neighbour.dx = -neighbour.dx
-				neighbour.dy = -neighbour.dy 
+ 				if (sqdist <= square(particle.radius + neighbour.radius)) {
+					//particle.vx = -particle.vx;
+					particle.vy = -particle.vy;
+					//neighbour.vx = -neighbour.vx;
+					neighbour.vy = -neighbour.vy;
 
-				balls.push(new Ball(canvas))  
-			} 
+					console.log(particle);
+					console.log(neighbour);
+				}
+			}
+		} else {
+			continue;
 		}
 	}
 
-	frame = window.requestAnimationFrame(render)
+	//  
+
+	for (let i = 0; i < balls.length; i++) {
+		balls[i].draw();
+		balls[i].displace();
+	}
+
+	frame = window.requestAnimationFrame(render);
 }
 
 // start on click
-// accelerate on subsequent clicks 
-document.addEventListener('click', e => {
-	e.preventDefault()	
-	frame = window.requestAnimationFrame(render)
-})
+// todo: accelerate on subsequent clicks
 
+let clicked = false;
+
+document.addEventListener("click", e => {
+	e.preventDefault();
+	if (clicked) {
+		window.cancelAnimationFrame(frame);
+	} else {
+		frame = window.requestAnimationFrame(render);
+	}
+	clicked = !clicked;
+});
