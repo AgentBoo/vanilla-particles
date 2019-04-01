804b8da37090dd0ea34ec1af3775934ae914a69c
agentboo <zuzi.han@gmail.com>
2019-04-01 01:58:39 -0400

    refactored simulation into own class

--- index.js
+++ index.js
@@ -2,48 +2,108 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Advanced_animations
 // https://www.html5rocks.com/en/tutorials/canvas/performance/
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-const { height, width } = canvas.parentElement.getBoundingClientRect();
-
-canvas.width = width;
-canvas.height = height;
-
-canvas.ctx = ctx;
-
-// https://stackoverflow.com/a/5365036
-function randomColor() {
-	return "#" + (((1 << 24) * Math.random()) | 0).toString(16);
-}
-
 function square(number) {
 	return number * number;
 }
 
+function magnitude(vector) {
+	return Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
+}
+
+function configure(canvasId) {
+	const canvas = document.getElementById(canvasId);
+	const ctx = canvas.getContext("2d");
+
+	const { height, width } = canvas.parentElement.getBoundingClientRect();
+
+	canvas.ctx = ctx;
+	canvas.width = width;
+	canvas.height = height;
+	canvas.fillStyle = "black";
+	canvas.strokeStyle = "black";
+
+	return canvas;
+}
+
+class Particles {
+	constructor(canvas) {
+		this.canvas = canvas;
+		this.particles = [];
+
+		this.running = false;
+		this.frameId = null;
+	}
+
+	start() {
+		this.seed(10);
+		this.render();
+	}
+
+	stop() {}
+
+	seed(N) {
+		for (let i = 0; i < N; i++) {
+			this.particles.push(new Ball(this.canvas, {}));
+		}
+	}
+
+	render() {
+		this.canvas.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+		for (let i = 0; i < this.particles.length; i++) {
+			this.particles[i].draw();
+			this.particles[i].displace();
+		}
+
+		this.frameId = window.requestAnimationFrame(this.render);
+	}
+}
+
+class Dot {
+	constructor(canvas, coordX, coordY) {
+		this.x = coordX;
+		this.y = coordY;
+		this.ctx = canvas.getContext("2d");
+	}
+
+	draw() {
+		this.ctx.beginPath();
+		this.ctx.arc(this.x, this.y, 1.5, 0, Math.PI * 2, true);
+		this.ctx.fillStyle = "red";
+		this.ctx.fill();
+	}
+}
+
 class Ball {
 	constructor(canvas, init) {
-		this.mass = Math.random() * 4 + 2;
+		this.m = 4; // mass
 		this.x = init.x || Math.random() * (canvas.width - 40) + 20; // position X
 		this.y = init.y || Math.random() * (canvas.height - 40) + 20; // position Y
 		this.vx = init.vx || Math.random() * 0.4 + 0.2; // velocity X
 		this.vy = init.vy || Math.random() * 0.4 + 0.2; // velocity Y
 
 		this.growth = {
-			enabled: true,
+			enabled: false,
 			radius: 0.1,
-			rate: 1 / this.mass,
-			factor: 3
+			rate: 1 / this.m,
+			factor: 20
 		};
+
+		this.canvas = canvas;
 	}
 
-	// the heavier the ball, the larger the radius
-
 	get radius() {
+		// the heavier the ball, the larger the radius
+
 		if (this.growth.enabled) {
+			if (this.growth.radius >= this.m) {
+				this.growth.enabled = false;
+			} else {
+				this.growth.radius += this.growth.rate / this.growth.factor;
+			}
 			return this.growth.radius;
 		} else {
-			return this.mass;
+			return this.m;
 		}
 	}
 
@@ -53,49 +113,25 @@ class Ball {
 
 	get outOfBoundsX() {
 		return (
-			this.x + this.radius + this.vx > canvas.width ||
-			this.x + this.radius + this.vx < this.diameter
+			this.x + this.vx + this.radius > canvas.width ||
+			this.x + this.vx + this.radius < this.diameter
 		);
 	}
 
 	get outOfBoundsY() {
 		return (
-			this.y + this.radius + this.vy > canvas.height ||
-			this.y + this.radius + this.vy < this.diameter
+			this.y + this.vy + this.radius > canvas.height ||
+			this.y + this.vy + this.radius < this.diameter
 		);
 	}
 
-	stopGrowth() {
-		this.growth.enabled = false;
-	}
-
-	squareDistanceTo(neighbour) {
-		return square(this.x - neighbour.x) + square(this.y - neighbour.y);
-	}
-
-	grow() {
-		if (this.growth.radius >= this.mass) {
-			return this.stopGrowth();
-		} else {
-			this.growth.radius += this.growth.rate / this.growth.factor;
-		}
-	}
-
 	draw() {
-		if (this.growth.enabled) {
-			this.grow();
-		}
-
-		canvas.ctx.beginPath();
-		canvas.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-		canvas.ctx.fillStyle = "black";
-		canvas.ctx.fill();
+		this.canvas.ctx.beginPath();
+		this.canvas.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		this.canvas.ctx.fill();
 	}
 
 	displace() {
-		this.x += this.vx;
-		this.y += this.vy;
-
 		if (this.outOfBoundsX) {
 			this.vx = -this.vx;
 		}
@@ -103,72 +139,25 @@ class Ball {
 		if (this.outOfBoundsY) {
 			this.vy = -this.vy;
 		}
+
+		this.x += this.vx;
+		this.y += this.vy;
 	}
 }
 
-function createBalls(N){
-	let balls = []
+/* __main__ */
 
-	for (let i = 0; i < N; i++) {
-		balls.push(new Ball(canvas, {}));
-	}
+let canvas = configure("canvas");
+let simulation = new Particles(canvas);
 
-	return balls
-}
+simulation.start();
 
-let balls = createBalls(8)
+document.addEventListener("click", event => {
+	event.preventDefault();
 
-// render
-let frame;
-
-function render() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	for (let i = 0; i < balls.length; i++) {
-		let particle = balls[i];
-
-		if (balls.length < 50) {
-			for (let j = i + 1; j < balls.length; j++) {
-				let neighbour = balls[j];
-
-				let sqdist = particle.squareDistanceTo(neighbour) 
-
- 				if (sqdist <= square(particle.radius + neighbour.radius)) {
-					//particle.vx = -particle.vx;
-					particle.vy = -particle.vy;
-					//neighbour.vx = -neighbour.vx;
-					neighbour.vy = -neighbour.vy;
-
-					console.log(particle);
-					console.log(neighbour);
-				}
-			}
-		} else {
-			continue;
-		}
-	}
-
-	//  
-
-	for (let i = 0; i < balls.length; i++) {
-		balls[i].draw();
-		balls[i].displace();
-	}
-
-	frame = window.requestAnimationFrame(render);
-}
-
-// start on click
-// todo: accelerate on subsequent clicks
-
-let clicked = false;
-
-document.addEventListener("click", e => {
-	e.preventDefault();
-	if (clicked) {
-		window.cancelAnimationFrame(frame);
+	if (simulation.running) {
+		simulation.stop();
 	} else {
-		frame = window.requestAnimationFrame(render);
+		simulation.start();
 	}
-	clicked = !clicked;
 });
